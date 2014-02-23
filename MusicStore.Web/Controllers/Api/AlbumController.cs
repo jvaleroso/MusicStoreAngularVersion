789f96b3731c4f3b0a3db521dae2f15ce9c04f48
Jayson Valeroso <valerosojayson@gmail.com>
2014-02-23 11:22:35 +0800

    Save changes. :)

--- MusicStore.Web/Controllers/Api/AlbumController.cs
+++ MusicStore.Web/Controllers/Api/AlbumController.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using AutoMapper;
 using MusicStore.Services;
-using MusicStore.Web.Models;
 
 namespace MusicStore.Web.Controllers.Api
 {
